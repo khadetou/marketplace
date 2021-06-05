e0d07a5eef6e01a2df67317efa2709bd99ffa08b
khadetou <khadetou96@gmail.com>
2021-06-05 19:14:44 +0000

    customer login

--- backend/routes/login.js
+++ backend/routes/login.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import auth from '../middleware/auth.js';
 import Trader from '../models/trader.js';
+import Customer from '../models/customer.js';
 import asyncHandler from 'express-async-handler';
 import {check, validationResult} from 'express-validator';
 import jsonwebtoken from 'jsonwebtoken';
@@ -11,7 +12,7 @@ const router = express.Router();
 
 
 
-//@route POST/api/login
+//@route POST/api/login/trader
 //@desc login as trader
 //@access public
 
@@ -57,6 +58,53 @@ router.post('/trader',[
         res.json({token})
     })
 }))
+//@route POST/api/login/customer
+//@desc login as customer
+//@access public
+
+router.post('/customer',[
+                check('email','Enter a valid email').isEmail(),
+                check('password', 'Password is required').exists()
+], asyncHandler(async(req, res)=>{
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const {email, password}= req.body;
+    let customer = await Customer.findOne({email});
+
+    //CHECK IF THE TRADER EXIST IN THE DATABASE 
+    if(!customer){
+        res.status(400).json({errors:[{msg: 'Invalid credentials'}]})
+    }
+
+    //CHECK IF THE PASSWORD EXIST
+    const isMatch = await bcryptjs.compare(password, customer.password);
+    if(!isMatch){
+        res.status(400).json({errors:[{msg: 'Invalid credentials'}]})
+    }
+
+    
+    //CHECK IF THE TRADER IS VERIFIED
+    if(!customer.isVerified){
+        res.status(400).json({msg: 'The email was not verified'})
+    }
+
+
+    //RETURN JSONWEBTOKEN
+    const preload ={
+        customer:{
+            id: customer.id
+        }
+    }
+    
+    jsonwebtoken.sign(preload, process.env.JWST, {expiresIn: '1d'}, (err, token)=>{
+        if(err) throw err;
+        res.json({token})
+    })
+}))
+
 
 
 //@route  Get/api/login/trader
@@ -67,5 +115,18 @@ router.get('/trader', auth, asyncHandler(async(req, res)=>{
   
     res.json(trader);
 }))
+
+
+//@route  Get/api/login/customer
+//@des get selfinfos 
+//@access Private
+router.get('/customer', auth, asyncHandler(async(req, res)=>{
+    const customer = await Customer.findById(req.customer.id).select('-password');
+  
+    res.json(customer);
+}))
+
+
+
 
 export default router;
